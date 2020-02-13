965f40c3d7019a2161f9148a472a59a6e54a8be8
Jan Wawruszczak <janwawruszczak1998@gmail.com>
2020-02-13 15:49:27 +0100

    added Async server

--- server.cpp
+++ server.cpp
@@ -2,8 +2,12 @@
 #include<iostream>
 #include<string>
 #include<boost/asio.hpp>
+#include<boost/bind.hpp>
+#include<boost/shared_ptr.hpp>
+#include<boost/enable_shared_from_this.hpp>
 
 using namespace boost::asio;
+
 
 //message about daytime
 std::string make_daytime_string(){
@@ -11,25 +15,119 @@ std::string make_daytime_string(){
     return std::ctime(&now);
 }
 
+class ConnectionHandler : public boost::enable_shared_from_this<ConnectionHandler>{
+private:
+    ip::tcp::socket socket_;
+    std::string message_ = "Hello there!\n";
+    char data[256];
+
+    ConnectionHandler(io_service& io_service)
+    :socket_(io_service)
+    {
+    }
+
+public:
+    typedef boost::shared_ptr<ConnectionHandler> pointer;
+
+    //creating new pointer of this class - factory
+    //need to be static - creating this in server constructor
+    static pointer create_pointer(io_service& io_service){
+        return pointer(new ConnectionHandler(io_service));
+    }
+
+    ip::tcp::socket& get_socket(){
+        return socket_;
+    }
+    
+    void start(){
+        
+        //reading
+        socket_.async_read_some(buffer(data, 256),
+            boost::bind(&ConnectionHandler::handle_read, 
+                shared_from_this(),
+                placeholders::error,
+                placeholders::bytes_transferred
+        ));
+        
+        //writing
+        socket_.async_write_some(buffer(message_, 256),
+            boost::bind(&ConnectionHandler::handle_write, 
+                shared_from_this(),
+                placeholders::error,
+                placeholders::bytes_transferred
+        ));
+        
+    }
+
+    void handle_read(const boost::system::error_code& error,
+                    size_t bytes_transferred){
+        if(!error){
+            std::cout << data << std::endl;
+        }
+        else{
+            std::cerr << "error" << error.message() << std::endl;
+            socket_.close();
+        }
+    }
+
+    void handle_write(const boost::system::error_code& error,
+                    size_t bytes_transferred){
+        if(error){
+            std::cerr << "error" << error.message() << std::endl;
+            socket_.close();
+        }
+        else{
+            std::cout << "Helo from server" << std::endl;
+        }
+    }
+};
+
+
+class Server{
+private:
+    ip::tcp::acceptor acceptor_;
+
+    void accept(){
+        //create a socket and initiates async accept operations
+        ConnectionHandler::pointer connection 
+            = ConnectionHandler::create_pointer(acceptor_.get_io_service());
+        
+        
+        //accept operation and wait
+        acceptor_.async_accept(connection->get_socket(),
+            boost::bind(&Server::handle_accept, this, connection,
+            placeholders::error));
+            
+    }
+
+public:
+    Server(io_service& io_service)
+    : acceptor_(io_service, ip::tcp::endpoint(ip::tcp::v4(), 8080))
+    {
+        accept();
+    }
+
+    void handle_accept(ConnectionHandler::pointer connection, 
+        const boost::system::error_code& error
+        ){
+
+        if(!error){
+            connection->start();
+        }
+        accept();
+
+    }
+};
+
 int main(){
     try{
         io_service io_service;
-
-        //acceptator needed to listen for new connections
-        ip::tcp::acceptor acceptator(io_service, ip::tcp::endpoint(ip::tcp::v4(), 8080)); //port 8080
-        while(true){
-            ip::tcp::socket socket(io_service);
-            acceptator.accept(socket);
-            //some message - current time
-            std::string message = make_daytime_string();
-
-            //when client accessing server
-            boost::system::error_code error;
-            write(socket, buffer(message), error);
-
-        }
+        Server server(io_service);
+        io_service.run();
     }
     catch(std::exception exception){
         std::cerr << exception.what() << std::endl;
     }
+
+    return 0;
 }
