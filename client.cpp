5fb84ccd021fe8dc1528bf9bcf2403b85fb1f05c
Jan Wawruszczak <janwawruszczak1998@gmail.com>
2020-02-17 19:47:34 +0100

    Chat is working now

--- client.cpp
+++ client.cpp
@@ -2,70 +2,107 @@
 #include <stdlib.h>
 #include <iterator>
 #include <algorithm>
+#include <sstream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
 
 
 using namespace boost::asio;
 
-  
-std::string getData(ip::tcp::socket& socket) 
-{ 
-    streambuf buf; 
-    read_until(socket, buf, "\n"); 
-    std::string data = buffer_cast<const char*>(buf.data()); 
-    return data; 
+
+class Client
+{
+public:
+    Client(boost::asio::io_service& io, boost::shared_ptr<ip::tcp::socket>  socket)
+    :   strand_(io),
+        timer1_(io, boost::posix_time::seconds(1)),
+        timer2_(io, boost::posix_time::seconds(1))
+    {
+        socket_ptr = socket;
+        std::cout << "You are connected!" << std::endl;
+    }
+     ~Client()
+    {
+        std::cout << "Connection lost" << std::endl;
+    }
+
+void TimeoutThread() {
+        while(true){
+           // std::cout << "reader" << std::endl;
+            getData();
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(00));
+        }
+    }
+
+    void WorkerThread() {
+        while(true){
+           // std::cout << "writer" << std::endl;
+            sendData();
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(10));
+        }
+    }
+
+     void run()
+    {
+        timeoutThread = boost::thread(boost::bind(&Client::TimeoutThread, this));
+        workerThread  = boost::thread(boost::bind(&Client::WorkerThread, this));
+        workerThread.join();
+        timeoutThread.join();
+    }
+
+    void getData() { 
+        streambuf buf; 
+        read_until(*socket_ptr, buf, ""); 
+        std::string data = buffer_cast<const char*>(buf.data()); 
+        std::cout << ": " << data << std::endl;
+
+        timer1_.expires_at(timer1_.expires_at() + boost::posix_time::seconds(1));
+        timer1_.async_wait(strand_.wrap(boost::bind(&Client::getData, this)));
+    } 
+
+    void sendData() { 
+
+        std::string message;
+        getline(std::cin, message);
+        write(*socket_ptr, 
+            buffer(message)); 
+
+        timer2_.expires_at(timer2_.expires_at() + boost::posix_time::seconds(1));
+        timer2_.async_wait(strand_.wrap(boost::bind(&Client::sendData, this)));
 } 
   
-void sendData(ip::tcp::socket& socket, const std::string& message) 
-{ 
-    write(socket, 
-          buffer(message + "\n")); 
-} 
+
+
+  private:
+    boost::asio::io_service::strand strand_;
+    boost::shared_ptr<ip::tcp::socket> socket_ptr;
+    boost::asio::deadline_timer timer1_;
+    boost::asio::deadline_timer timer2_;
+
+    boost::thread timeoutThread;
+    boost::thread workerThread;
+
+};
+
   
 int main(int argc, char* argv[]) 
 { 
+
     io_service io_service; 
-    // socket creation 
-    ip::tcp::socket client_socket(io_service); 
+    boost::shared_ptr<ip::tcp::socket> socket_ptr(new ip::tcp::socket(io_service));
   
-    client_socket 
-        .connect( 
+    socket_ptr->
+        connect( 
             ip::tcp::endpoint( 
                 ip::address::from_string("127.0.0.1"), 
                 atoi(argv[1]))); 
   
-    // Getting username from user 
-    std::string reply = "Hi!", response; 
-  
-    // Sending username to another end 
-    // to initiate the conversation 
-    sendData(client_socket, reply); 
-  
-    // Infinite loop for chit-chat 
-    while (true) { 
-  
-        // Fetching response 
-        response = getData(client_socket); 
-  
-        // Popping last character "\n" 
-        response.pop_back(); 
-  
-        // Validating if the connection has to be closed 
-        if (response == "exit") { 
-            std::cout << "Connection terminated" << std::endl; 
-            break; 
-        } 
-        std::cout << ": " << response << std::endl; 
-  
-        // Reading new message from input stream 
-        getline(std::cin, reply); 
-        sendData(client_socket, reply); 
-  
-        if (reply == "exit") 
-            break; 
-    } 
+    Client client(io_service, socket_ptr);
+    io_service.run();
+    client.run();
+
     return 0; 
 } 
--- server.cpp
+++ server.cpp
@@ -1,78 +1,125 @@
-#include<ctime>
-#include<iostream>
-#include<string>
-#include<list>
-#include<queue>
-#include<boost/asio.hpp>
-#include<boost/bind.hpp>
-#include<boost/shared_ptr.hpp>
-#include<boost/enable_shared_from_this.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <iterator>
+#include <algorithm>
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
 
-using namespace boost::asio;   
-// Driver program for receiving data from buffer 
-std::string getData(ip::tcp::socket& socket) 
-{ 
+
+
+using namespace boost::asio;
+
+
+class Server
+{
+public:
+  Server(boost::asio::io_service& io, boost::shared_ptr<ip::tcp::socket>  socket)
+    : strand_(io),
+        timer1_(io, boost::posix_time::seconds(10)),
+        timer2_(io, boost::posix_time::seconds(10))
+    {
+    socket_ptr = socket;
+    std::cout << "You are connected!" << std::endl;
+    }
+
+  ~Server()
+  {
+    std::cout << "Connection lost" << std::endl;
+  }
+
+  void getData() { 
+   // std::cout << "read" << std::endl;
     streambuf buf; 
-    read_until(socket, buf, "\n"); 
+    read_until(*socket_ptr, buf, ""); 
     std::string data = buffer_cast<const char*>(buf.data()); 
-    return data; 
+    std::cout << ": " << data << std::endl;
+    
+    timer1_.expires_at(timer1_.expires_at() + boost::posix_time::seconds(1));
+    timer1_.async_wait(strand_.wrap(boost::bind(&Server::getData, this)));
 } 
-  
-// Driver program to send data 
-void sendData(ip::tcp::socket& socket, const std::string& message) 
-{ 
-    write(socket, 
-          buffer(message + "\n")); 
+
+void sendData() { 
+    std::string message;
+    getline(std::cin, message);
+    write(*socket_ptr, 
+          buffer(message)); 
+    
+    timer2_.expires_at(timer2_.expires_at() + boost::posix_time::seconds(1));
+    timer2_.async_wait(strand_.wrap(boost::bind(&Server::sendData, this)));
+    
 } 
+
+void TimeoutThread() {
+        while(true){
+            getData();
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+        }
+    }
+
+    void WorkerThread() {
+        while(true){
+            sendData();
+            boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+        }
+    }
+
+    
+    void run()
+    {
+        timeoutThread = boost::thread(boost::bind(&Server::TimeoutThread, this));
+        workerThread  = boost::thread(boost::bind(&Server::WorkerThread, this));
+        workerThread.join();
+        timeoutThread.join();
+    }
   
-int main(int argc, char* argv[]) 
+
+
+private:
+    boost::asio::io_service::strand strand_;
+    boost::shared_ptr<ip::tcp::socket> socket_ptr;
+    boost::asio::deadline_timer timer1_;
+    boost::asio::deadline_timer timer2_;
+
+    boost::thread timeoutThread;
+    boost::thread workerThread;
+
+};
+  
+
+  
+
+int main(int argc, char* argv[])
 { 
-    io_service io_service; 
-  
+    
+     io_service io_service; 
+    
     // Listening for any new incomming connection 
     // at port 8080 with IPv4 protocol 
+    
     ip::tcp::endpoint endpoint(ip::tcp::v4(), atoi(argv[1]));
+
     ip::tcp::acceptor acceptor_server( 
         io_service, endpoint); 
-  
-    // Creating socket object 
-    ip::tcp::socket server_socket(io_service); 
-  
+
+    boost::shared_ptr<ip::tcp::socket> socket_ptr(new ip::tcp::socket(io_service));
     // waiting for connection 
-    acceptor_server.accept(server_socket); 
-  
-    // Reading username 
-    std::string u_name = getData(server_socket); 
-    // Removing "\n" from the username 
-    u_name.pop_back(); 
-  
-    // Replying with default mesage to initiate chat 
-    std::string response, reply; 
-    reply = "Hello there!";
-    sendData(server_socket, reply); 
-  
-    while (true) { 
-  
-        // Fetching response 
-        response = getData(server_socket); 
-  
-        // Popping last character "\n" 
-        response.pop_back(); 
-  
-        // Validating if the connection has to be closed 
-        if (response == "exit") { 
-            std::cout << "User left!" << std::endl; 
-            break; 
-        } 
-        std::cout << ": " << response << std::endl; 
-  
-        // Reading new message from input stream 
-        
-        getline(std::cin, reply); 
-        sendData(server_socket, reply); 
-  
-        if (reply == "exit") 
-            break; 
-    } 
+    
+    try{
+        acceptor_server.accept(*socket_ptr); 
+    }
+    catch(std::exception e){
+        std::cout << e.what();
+    }
+
+    
+
+    Server server(io_service, socket_ptr);
+
+    io_service.run();
+    server.run();
+
     return 0; 
 } 
